f605333f6f2ea4ea9699cb3535f8fdead4b7c0f8
Eugene <93394578+EuGeNe-Las@users.noreply.github.com>
2022-07-24 00:21:15 +0300

    Added fps

--- HandsTracker.py
+++ HandsTracker.py
@@ -4,20 +4,29 @@ import time
 
 cap = cv2.VideoCapture(0)
 
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpDraw = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+prev_time = 0
+cur_time = 0
 
 while True:
     success, img = cap.read()
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    results = hands.process(imgRGB)
+    img_RGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img_RGB)
     #print(results.multi_hand_landmarks)
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms)
+            mp_draw.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS)
 
+
+    cur_time = time.time()
+    fps = 1/(cur_time - prev_time)
+    prev_time = cur_time
+
+    cv2.putText(img, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255,255,0), 3)
 
     cv2.imshow('image', img)
     cv2.waitKey(1)
