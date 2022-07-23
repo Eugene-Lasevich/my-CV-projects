474c99b9f2906121bcc445c168e474eb3e5cb5e8
Eugene <93394578+EuGeNe-Las@users.noreply.github.com>
2022-07-23 23:56:55 +0300

    Added recognition points

--- HandsTracker.py
+++ HandsTracker.py
@@ -4,7 +4,20 @@ import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
 while True:
-    succes, img = cap.read()
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    #print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms)
+
+
     cv2.imshow('image', img)
     cv2.waitKey(1)
